628e0d4ccc7b3d486eed3abe9abe691cf5e0cfae
RTookey <rachel.tookey@softwire.com>
2025-04-28 16:29:19 +0100

    Test: added tests for bishop and queen

--- Chessington.GameEngine.Tests/Pieces/BishopTests.cs
+++ Chessington.GameEngine.Tests/Pieces/BishopTests.cs
@@ -1,9 +1,71 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using Chessington.GameEngine.Pieces;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Chessington.GameEngine.Tests.Pieces
 {
     [TestFixture]
     public class BishopTests
-    { 
+    {
+        [Test]
+        public void BishopCanMoveFourDirections()
+        {
+            var board = new Board();
+            var bishop = new Bishop(Player.White);
+            board.AddPiece(Square.At(3, 3), bishop);
+            
+            var moveList = bishop.GetAvailableMoves(board);
+
+            moveList.Should().HaveCount(13);
+        }
+
+        [Test]
+        public void BishopMovesDiagonally()
+        {
+            var board = new Board();
+            var bishop = new Bishop(Player.White);
+            board.AddPiece(Square.At(0, 0), bishop);
+            
+            var pawn = new Pawn(Player.Black);
+            board.AddPiece(Square.At(1, 1), pawn);
+            
+            var moveList = bishop.GetAvailableMoves(board);
+
+            var positionToMove = moveList.ToList()[0];
+            positionToMove.Row.Should().Be(1);
+            positionToMove.Col.Should().Be(1);
+        }
+
+        [Test]
+        public void BishopCanTakeOppositePieces()
+        {
+            var board = new Board();
+            var bishop = new Bishop(Player.White);
+            board.AddPiece(Square.At(0, 0), bishop);
+            
+            var pawn = new Pawn(Player.Black);
+            board.AddPiece(Square.At(1, 1), pawn);
+            
+            var moveList = bishop.GetAvailableMoves(board);
+
+            moveList.Should().HaveCount(1);
+        }
+
+        [Test]
+        public void BishopCannotTakeOwnPieces()
+        {
+            var board = new Board();
+            var bishop = new Bishop(Player.White);
+            board.AddPiece(Square.At(0, 0), bishop);
+            
+            var pawn = new Pawn(Player.White);
+            board.AddPiece(Square.At(1, 1), pawn);
+            
+            var moveList = bishop.GetAvailableMoves(board);
+
+            moveList.Should().HaveCount(0);
+        }
+        
     }
 }
--- Chessington.GameEngine.Tests/Pieces/QueenTests.cs
+++ Chessington.GameEngine.Tests/Pieces/QueenTests.cs
@@ -1,9 +1,54 @@
-﻿using NUnit.Framework;
+﻿using Chessington.GameEngine.Pieces;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Chessington.GameEngine.Tests.Pieces
 {
     [TestFixture]
     public class QueenTests
     {
+        [Test]
+        public void QueenCanMoveAllDirections()
+        {
+            var board = new Board();
+            var queen = new Queen(Player.White);
+            board.AddPiece(Square.At(3, 3), queen);
+            
+            var moveList = queen.GetAvailableMoves(board);
+
+            moveList.Should().HaveCount(27);
+        }
+        
+        [Test]
+        public void QueenCanTakeOppositePieces()
+        {
+            var board = new Board();
+            var queen = new Queen(Player.White);
+            board.AddPiece(Square.At(3, 3), queen);
+            
+            var pawn = new Pawn(Player.Black);
+            board.AddPiece(Square.At(3, 0), pawn);
+            
+            var moveList = queen.GetAvailableMoves(board);
+
+            moveList.Should().HaveCount(27);
+            
+        }
+
+        [Test]
+        public void QueenCannotTakeOwnPieces()
+        {
+            var board = new Board();
+            var queen = new Queen(Player.White);
+            board.AddPiece(Square.At(3, 3), queen);
+            
+            var pawn = new Pawn(Player.White);
+            board.AddPiece(Square.At(3, 0), pawn);
+            
+            var moveList = queen.GetAvailableMoves(board);
+
+            moveList.Should().HaveCount(26);
+
+        }
     }
 }
